131847d480f2ba83620f89b9885dc017cf147c91
Prasandeep <prasandeepp@gmail.com>
2023-01-16 09:35:08 +0530

    backend part done!

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const router =  require('express').Router();
 const Post = require('../models/Post.js');
-
+const User = require('../models/User.js');
 // create a post
 router.post('/', async(req, res)=>{
     const newPost = new Post(req.body)
